ddde860bb06bad2a5a4909c815d79f8dbc54e175
yuniuniu <chentienan@kanyun.com>
2024-10-11 23:48:59 +0800

    feat: sup parse m4a format audio bytes

--- echo_journey/audio/speech_to_text/kanyun.py
+++ echo_journey/audio/speech_to_text/kanyun.py
@@ -39,10 +39,12 @@ class Kanyun(SpeechToText, Singleton):
 
         if platform == "web":
             audio = self._convert_webm_to_wav(audio_bytes, False)
-        elif platform == "mobile":
-            pass
+        elif platform == "ios":
+            audio = self._convert_m4a_to_wav(audio_bytes, False)
+        elif platform == "android":
+            audio = self._convert_webm_to_wav(audio_bytes, False)
         else:
-            audio = self._convert_bytes_to_wav(audio_bytes, False)
+            raise ValueError(f"Unsupported platform: {platform}")
 
         wav_data = BytesIO(audio.get_wav_data())
         wav_data.name = "SpeechRecognition_audio.wav"
@@ -72,6 +74,14 @@ class Kanyun(SpeechToText, Singleton):
         webm_audio = webm_audio.set_frame_rate(16000)
         wav_data = io.BytesIO()
         webm_audio.export(wav_data, format="s16le", codec="pcm_s16le")
+        return self._convert_bytes_to_wav(wav_data.getvalue(), local=local)
+    
+    def _convert_m4a_to_wav(self, m4a_data, local=True):
+        m4a_audio = AudioSegment.from_file(io.BytesIO(m4a_data), format="m4a")
+        m4a_audio = m4a_audio.set_channels(1)
+        m4a_audio = m4a_audio.set_frame_rate(16000)
+        wav_data = io.BytesIO()
+        m4a_audio.export(wav_data, format="wav", codec="pcm_s16le")
         return self._convert_bytes_to_wav(wav_data.getvalue(), local=local)
 
     def _convert_bytes_to_wav(self, audio_bytes, local=True):
