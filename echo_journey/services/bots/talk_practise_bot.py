87f55940e3e78f215d58f5de0940c2d6f36977ca
yuniuniu <chentienan@kanyun.com>
2024-10-13 17:20:06 +0800

    feat: make history more complete

--- echo_journey/services/bots/talk_practise_bot.py
+++ echo_journey/services/bots/talk_practise_bot.py
@@ -24,8 +24,8 @@ class TalkPractiseBot():
         self.talk_practise_bot.add_user_msg_to_cur({"role": "user", "content": "这个场景的练习结束"})
         teacher_info = await self.context.execute()
         await self.ws_msg_handler.send_tutor_message(text=teacher_info["teacher"])
-    
-    async def generate_practise_reply(self, student_status, student_text):
+        
+    def build_input_by(self, student_status, student_text):
         format_dict = {}
         format_dict["SCENE"] = self.practise_progress.get_scene()
         format_dict["PLAN"] = self.practise_progress.get_plan()
@@ -35,11 +35,21 @@ class TalkPractiseBot():
         format_dict["STUDENT_STATUS"] = student_status
         format_dict["STUDENT"] = student_text
         logger.info(f"format_dict: {format_dict}")
-        
         user_msg = self.context.cur_visible_assistant.content.user_prompt_prefix.format(**format_dict)
+        return user_msg
+    
+    async def generate_practise_reply(self, student_status, student_text):
+        user_msg = self.build_input_by(student_status, student_text)
         self.context.add_user_msg_to_cur({"role": "user", "content": user_msg})
         teacher_info = await self.context.execute()
         return teacher_info
+    
+    def add_suggestion_to_context(self, suggestions):
+        import json
+        user_msg = self.build_input_by("学生的发音存在问题错误", "老师帮我纠下音吧")
+        self.context.add_user_msg_to_cur({"role": "user", "content": user_msg})
+        assistant_msg = json.dumps({"skip": False, "teacher": suggestions}, ensure_ascii=False)
+        self.context.add_assistant_msg_to_cur({"role": "assistant", "content": assistant_msg})
         
     async def send_practise_msg(self, student_status, student_text, platform):
         teacher_info = await self.generate_practise_reply(student_status, student_text)
