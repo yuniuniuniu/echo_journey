3971393fa85d81f75d7d1cf91d18bce4354ec40f
yuniuniu <chentienan@kanyun.com>
2024-10-18 09:56:49 +0800

    feat: sup restful by deviceid

--- echo_journey/api/restful_routes.py
+++ echo_journey/api/restful_routes.py
@@ -1,9 +1,20 @@
 import json
 
-from fastapi import APIRouter, File, HTTPException, UploadFile
+from fastapi import APIRouter, File, HTTPException, Query, UploadFile
 
 router = APIRouter()
 
-@router.post("/diagnoses")
-async def upload_image(file: UploadFile = File(...)):
-    pass
+@router.get("/titles")
+async def get_title(deviceId: str = Query(default=None)):
+    return [{
+      "name": '瓜瓜',
+      "description": '今天有什么想聊的话题？',
+      "scene": 'talk',
+      "update": False,
+    },
+    {
+      "name": '斗斗',
+      "description": '奖励你一大挑战？',
+      "scene": 'exercises',
+      "update": False,
+    }]
