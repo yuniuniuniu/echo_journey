0b8f3724f4307feefe347b6d4ded2754afd520de
yuniuniu <chentienan@kanyun.com>
2024-10-28 21:26:25 +0800

    feat: sup create user & user login

--- echo_journey/common/utils.py
+++ echo_journey/common/utils.py
@@ -182,3 +182,13 @@ def encode_image(image_path):
 
 def encode_image_bytes(image_in_bytes: bytes):
     return base64.b64encode(image_in_bytes).decode("utf-8")
+
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
